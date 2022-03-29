8e8d2dc2626544fc8b03a98aa48ff58b42fdf67d
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-03-29 10:31:35 +0530

    Create 7. Reverse Integer.cpp

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans=0;
+        while(x!=0){
+            int dig = x%10;
+            while((ans>INT_MAX/10) || (ans<INT_MIN/10)){
+                return 0;
+            }
+            ans=(ans*10)+dig;
+            x=x/10;
+        }
+        return ans;
+    }
+};
