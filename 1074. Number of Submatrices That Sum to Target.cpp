44883312e0ad9a47108f012b0425df71e55403e6
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2022-07-18 10:19:37 +0530

    Create 1074. Number of Submatrices That Sum to Target.cpp

---
+++ 1074. Number of Submatrices That Sum to Target.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        
+    
+	int res = 0, m = size(matrix), n = size(matrix[0]);
+	for (int row = 0; row < m; row++)
+		for (int col = 1; col < n; col++)
+			matrix[row][col] += matrix[row][col - 1];
+	//         cursum, occurences
+	unordered_map<int, int> mp;
+	for (int colStart = 0; colStart < n; colStart++) {
+		for (int colEnd = colStart; colEnd < n; colEnd++) {
+			int cursum = 0;
+			mp = {{0, 1}};
+			for(int row = 0; row < m; row++){
+				cursum += matrix[row][colEnd] - (colStart ? matrix[row][colStart - 1] : 0);    
+				// mp[sum-target] will give number of submatrices found having sum as 'sum - target'
+				res += mp[cursum - target];
+				mp[cursum]++;
+			}
+		}
+	}
+	return res;    
+}
+    
+};
