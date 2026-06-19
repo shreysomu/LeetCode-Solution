7da9d6b2b8641308944062ee421d8cd1da53701f
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-06-19 20:57:11 +0530

    Create 1732. Find the Highest Altitude.java

---
+++ 1732. Find the Highest Altitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int currentAltitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            currentAltitude += g;
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+
+        return maxAltitude;
+    }
+}
