36e62cfdca86ead2c734658ed07aa2b5cf04f245
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-09-02 10:27:46 +0530

    Create 3875. Construct Uniform Parity Array I.java

---
+++ 3875. Construct Uniform Parity Array I.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+
+    //     Even - Even = Even
+    //     Odd - Odd   = Even
+
+    //    Even - Odd  = Odd
+    //    Odd - Even  = Odd
+        return true;
+    }
+}
